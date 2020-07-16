839aa864502f05ddec4c70c041a9f8fc137c5bde
atsushitakano <atsusi0123sp@gmail.com>
2020-07-16 16:51:47 +0900

    questionモデルのcreateアクション①
    ここではquestionモデルのcreateアクションを実装。
    正解と選択肢はquestion_idに紐付けて別テーブルに保存する仕様

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,4 +1,22 @@
 class QuestionsController < ApplicationController
   def index
   end
+
+  def new
+    @question = Question.new
+  end
+
+  def create
+    @question = Question.new(question_params)
+    @question.save
+    redirect_to root_path
+  end
+
+
+
+  private
+
+  def question_params
+    params.require(:question).permit(:quiz,:explain_sentence)
+  end
 end
